374037bf1f9a8a556a78c59bb13ccc4376891984
Jesse Mills <jessemills@MacBook-Pro.local>
2016-06-07 15:58:05 -0400

    Adding a static view folder and getting it to render
    Just adding a static view and getting it to render something

---
+++ src/main/java/change/Controllers/IndexController.java
@@ -0,0 +1,16 @@
+package change;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
