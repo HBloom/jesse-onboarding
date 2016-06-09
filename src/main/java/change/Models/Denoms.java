34ab778f2e6814b8ed813fd98d7a0168ea2628fc
Jesse Mills <jessemills1989@gmail.com>
2016-06-09 16:18:32 -0400

    Updating the printAll combos method to return better data
    This method now returns a more sane data structure with better
    info than previously it did

--- src/main/java/change/Models/Denoms.java
+++ src/main/java/change/Models/Denoms.java
@@ -15,38 +15,44 @@ public class Denoms {
         return totals;
     }
 
-    public static ArrayList printAll(int iter, ArrayList<Map> denom, Double cents, ArrayList vals, ArrayList totals) {
+    public static ArrayList printAll(int iter, ArrayList<Map> denom, Double cents, ArrayList<Map<String, Integer>> vals, ArrayList totals) {
         ArrayList denomins = totals;
-        System.out.println(cents.getClass());
         if (cents == 0.0) {
-            denomins.add(vals);
+            ArrayList<Map<String, Integer>> newArrList = new ArrayList<Map<String, Integer>>(vals);
+
+            denomins.add(newArrList);
             if (iter == 1) {
                 return denomins;
             }
             return denomins;
         }
-        if (iter == (denom.size())) {
+        if (iter == denom.size()) {
 
             return denomins;
         }
-        Object myVar = denom.get(iter).get("amount");
-        // System.out.println("The current denom is: " + myVar);
-        Double currdenom = (Double) myVar;
+        Object thisAmount = denom.get(iter).get("amount");
+        Object thisName = denom.get(iter).get("name");
+        String denomName = (String) thisName;
+        Double currdenom = (Double) thisAmount;
         for (int i = 0; i <= (cents / currdenom); i++) {
+
+            Map myMap = new HashMap();
+            myMap.put("name", denomName);
+            myMap.put("number", i);
+            myMap.put("value", currdenom);
+
             if (vals.isEmpty()) {
-              // System.out.println("Vals was empty so added " + i + " on iter: " + iter);
-              vals.add(i);
+                vals.add(myMap);
             } else if (vals.size() <= iter) {
-              // System.out.println("Val size and iter are equal, added: " + i + "at iter: " + iter);
-              vals.add(i);
+                vals.add(myMap);
             } else {
-              // System.out.println("Got to the else part and and added: " + i + "at iter: "+ iter);
-              vals.set(iter, i);
+                vals.set(iter, myMap);
             }
 
-            // System.out.println(vals);
 
-            printAll(iter + 1, denom, cents - i * currdenom, vals, denomins);
+            Double newCents = cents - i * currdenom;
+
+            printAll(iter + 1, denom, newCents, vals, denomins);
         }
 
         return denomins;
