72f0377baba638acf70c20ebc7ff17b3f8216ef4
Jesse Mills <jessemills1989@gmail.com>
2016-06-09 17:19:33 -0400

    First little bits into a front end that hits my endpoint sans css
    The beginnings of a front end to hit the endpoint. Nothing at all fancy
    at the moment (and probably won't get a ton fancier :S)

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,37 @@
 (function() {
-  console.log("hello");
+
+    $("#submit-button").click(function() {
+
+        var coinNames = $('input[name="coinNames"]').map(function() {
+            return $(this).val();
+        }).get().join();
+
+        var coinValues = $('input[name="coinValues"]').map(function() {
+            return $(this).val();
+        }).get().join();
+
+        var amount = $('input[name="coinAmount"]').val();
+
+        $.ajax({
+            url: "/change",
+            method: "POST",
+            data: {
+                "coinNames": coinNames,
+                "coinValues": coinValues,
+                "totalAmount": amount
+            }
+
+        }).then(
+            function(response) {
+                console.log(response);
+            },
+            function(error) {
+                console.log(error);
+            }
+        );
+
+
+    });
+
+
 })();
